7115a82aa6adf960c6f200ac63750493c38f7643
ItsMeRayyan <runstarray@gmail.com>
2026-07-01 09:18:49 +0100

    Delete docs/script.js

--- docs/script.js
+++
@@ -1,8 +0,0 @@
-console.log("VolT OS loaded");
-
-// Flip cards on click
-document.querySelectorAll(".card").forEach(card => {
-    card.addEventListener("click", () => {
-        card.classList.toggle("flipped");
-    });
-});
