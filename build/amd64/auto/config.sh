1208c0d19d507879baeca46255650068a55c6800
ItsMeRayyan <runstarray@gmail.com>
2026-07-02 12:08:36 +0300

    Refactor config.sh for architecture variable and mirrors

--- build/amd64/auto/config.sh
+++ build/amd64/auto/config.sh
@@ -1,11 +1,19 @@
+#!/bin/bash
+
+set -e
+
+ARCH=amd64
+
 lb config \
   --mode debian \
   --distribution bookworm \
-  --architecture amd64 \
+  --architecture ${ARCH} \
   --binary-images iso-hybrid \
   --debian-installer live \
   --archive-areas "main contrib non-free non-free-firmware" \
-  --mirror-bootstrap https://deb.debian.org/debian/ \
-  --mirror-chroot https://deb.debian.org/debian/ \
-  --mirror-binary https://deb.debian.org/debian/ \
-  --security true
+  --mirror-bootstrap http://deb.debian.org/debian/ \
+  --mirror-chroot http://deb.debian.org/debian/ \
+  --mirror-binary http://deb.debian.org/debian/ \
+  --security true \
+  --security-suite bookworm-security \
+  --bootappend-live "boot=live components quiet splash"
